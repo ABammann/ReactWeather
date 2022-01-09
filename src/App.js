f650e817efaad2bd2601471e0e1d0985642257c0
ABammann <87564425+ABammann@users.noreply.github.com>
2022-01-09 12:54:50 +0100

    Bunches of changes
    I gotta get more used to suing GItHub

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
-import SearchEngine from "./Search";
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather Forecast</h1>
-        <SearchEngine className="App-search" />
-        <p className="App-footer">
-          Code made by Alessandra Bammann. Check it at{" "}
-          <a href="https://github.com/ABammann/ReactWeather">GitHub</a>
-        </p>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Antwerp" />
+
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/alessandra-bammann-327983a1/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+          >
+            Alessandra Bammann
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/ABammann/ReactWeather"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://zealous-lamport-915a56.netlify.app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
